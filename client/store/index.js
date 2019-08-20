afb780d16f3775550d6356d8bd7a7b0e9f5d8508
calvin ho <calvinho1214@gmail.com>
2019-08-20 15:09:37 -0400

    reducer and axios install

--- client/store/index.js
+++ client/store/index.js
@@ -2,8 +2,9 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import Product from '../components/product'
 
-const reducer = combineReducers()
+const reducer = combineReducers({Product})
 const middleware = composeWithDevTools(
     applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
   )
