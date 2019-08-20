627d04e55b05797fbf0814b81645aae36fada332
calvin ho <calvinho1214@gmail.com>
2019-08-20 14:50:50 -0400

    added a routes.js with a routes class

---
+++ client/routes.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {withRouter, Route, Switch} from 'react-router-dom'
+import {connect} from 'react-redux'
+
+class Routes extends Component {
+    render(){
+
+    }
+}
+
+export default Routes
